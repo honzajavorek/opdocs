50cef7efe8bba7aff617ce9ee7c3d4093b455dec
Honza Javorek <mail@honzajavorek.cz>
2024-11-13 20:58:16 +0100

    initial implementation

---
+++ README.md
@@ -0,0 +1,22 @@
+# opdocs
+
+Use 1Password as your home docs
+
+## Installation
+
+1. Install the [1Password CLI](https://developer.1password.com/docs/cli/)
+1. Install this tool as `pipx install 'git+https://github.com/honzajavorek/homedocs.git'`
+
+## Usage
+
+```
+$ opdocs
+Read https://support.1password.com/markdown/ before editing!
+ 1. Family Finances
+ 2. Last Will
+Enter number of the one you want to edit:
+```
+
+You enter a number and `opdocs` opens VS Code with the content of the note for you to edit it. If you save and close the changes, the edits will be uploaded back to 1Password.
+
+The title of the note is included in the file as the H1 and changes to it will propagate. Use Markdown preview in VS Code to see what the note will look like, but mind that 1Password [supports only subset of the formatting](https://support.1password.com/markdown/).
---
+++ opdocs/cli.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+import json
+from operator import itemgetter
+import subprocess
+import tempfile
+
+import click
+
+
+@click.command()
+@click.option("--family", "vault", flag_value="Shared", help="Use the Shared vault", default=True)
+@click.option("--me", "vault", flag_value="Private", help="Use the Private vault")
+def main(vault: str):
+    click.echo("Read https://support.1password.com/markdown/ before editing!")
+
+    result = subprocess.run([
+        "op", "item", "list",
+        "--format", "json",
+        "--categories", "Secure Note",
+        "--vault", vault
+    ], check=True, text=True, capture_output=True)
+    items = sorted(json.loads(result.stdout), key=itemgetter("title"))
+
+    choices = {str(i + 1): item for i, item in enumerate(items)}
+    for n, item in choices.items():
+        updated_on = datetime.fromisoformat(item['updated_at']).strftime('%Y-%m-%d')
+        click.echo(f"{n:>2}. {item['title']} ({updated_on})")
+    chosen_n = click.prompt("Enter number of the one you want to edit", type=click.Choice(list(choices.keys())), show_choices=False)
+    item = choices[chosen_n]
+
+    result = subprocess.run([
+        "op", "item", "get", item['id'],
+        "--format", "json",
+        "--fields", "notesPlain",
+        "--vault", vault
+    ], check=True, text=True, capture_output=True)
+    field = json.loads(result.stdout)
+    value = field["value"].strip()
+    title = item['title']
+    note = f"# {title}\n\n{value}"
+
+    with tempfile.NamedTemporaryFile(mode="w+") as f:
+        f.write(note)
+        f.seek(0)
+        subprocess.run(["code", "-w", f.name], check=True)
+        f.seek(0)
+        edited_note = f.read().strip()
+
+    title_part, value_part = edited_note.split("\n", 1)
+    edited_title = title_part.lstrip("#").strip()
+    edited_value = value_part.strip()
+
+    args = []
+    if edited_title != title:
+        args.extend(["--title", edited_title])
+    if edited_value != value:
+        args.extend([f"notesPlain={edited_value}"])
+    if args:
+        subprocess.run([
+            "op", "item", "edit", item['id'],
+            "--vault", vault,
+            *args
+        ], check=True, text=True, capture_output=True)
