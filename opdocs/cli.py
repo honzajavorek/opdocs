52ec4faccde591dbeb2c12c0d2a9c21b9838ac08
Honza Javorek <mail@honzajavorek.cz>
2024-11-22 13:26:54 +0100

    support generating pdfs

--- opdocs/cli.py
+++ opdocs/cli.py
@@ -1,17 +1,34 @@
+from dataclasses import dataclass
 from datetime import datetime
+from email.policy import default
 import json
 from operator import itemgetter
+from pathlib import Path
+import shutil
 import subprocess
 import tempfile
 
 import click
 
 
+@dataclass
+class Note:
+    value: str
+    title: str
+    document: str
+
+
 @click.group(invoke_without_command=True)
-@click.option("--family", "vault", flag_value="Shared", help="Use the Shared vault", default=True)
+@click.option(
+    "--family", "vault", flag_value="Shared", help="Use the Shared vault", default=True
+)
 @click.option("--me", "vault", flag_value="Private", help="Use the Private vault")
 @click.pass_context
 def main(context: click.Context, vault: str):
+    if not shutil.which("op"):
+        click.echo("Not installed: op (1Password CLI)", err=True)
+        raise click.Abort()
+
     context.obj = {"vault": vault}
     if context.invoked_subcommand is None:
         context.invoke(edit)
@@ -20,54 +37,124 @@ def main(context: click.Context, vault: str):
 @main.command()
 @click.pass_context
 def edit(context: click.Context):
-    click.echo("Read https://support.1password.com/markdown/ before editing!")
+    click.echo("Read https://support.1password.com/markdown/ before editing!", err=True)
 
     vault = context.obj["vault"]
-    result = subprocess.run([
-        "op", "item", "list",
-        "--format", "json",
-        "--categories", "Secure Note",
-        "--vault", vault
-    ], check=True, text=True, capture_output=True)
-    items = sorted(json.loads(result.stdout), key=itemgetter("title"))
-
-    choices = {str(i + 1): item for i, item in enumerate(items)}
-    for n, item in choices.items():
-        updated_on = datetime.fromisoformat(item['updated_at']).strftime('%Y-%m-%d')
-        click.echo(f"{n:>2}. {item['title']} ({updated_on})")
-    chosen_n = click.prompt("Enter number of the one you want to edit", type=click.Choice(list(choices.keys())), show_choices=False)
-    item = choices[chosen_n]
-
-    result = subprocess.run([
-        "op", "item", "get", item['id'],
-        "--format", "json",
-        "--fields", "notesPlain",
-        "--vault", vault
-    ], check=True, text=True, capture_output=True)
-    field = json.loads(result.stdout)
-    value = field["value"].strip()
-    title = item['title']
-    note = f"# {title}\n\n{value}"
+    item = prompt_item(vault)
+    note = get_note(vault, item)
 
     with tempfile.NamedTemporaryFile(mode="w+") as f:
-        f.write(note)
+        f.write(note.document)
         f.seek(0)
         subprocess.run(["code", "-w", f.name], check=True)
         f.seek(0)
-        edited_note = f.read().strip()
+        edited_document = f.read().strip()
 
-    title_part, value_part = edited_note.split("\n", 1)
+    title_part, value_part = edited_document.split("\n", 1)
     edited_title = title_part.lstrip("#").strip()
     edited_value = value_part.strip()
 
     args = []
-    if edited_title != title:
+    if edited_title != note.title:
         args.extend(["--title", edited_title])
-    if edited_value != value:
+    if edited_value != note.value:
         args.extend([f"notesPlain={edited_value}"])
     if args:
-        subprocess.run([
-            "op", "item", "edit", item['id'],
-            "--vault", vault,
-            *args
-        ], check=True, text=True, capture_output=True)
+        subprocess.run(
+            [
+                "op",
+                "item",
+                "edit",
+                item["id"],
+                "--vault",
+                vault,
+                *args,
+            ],
+            check=True,
+            text=True,
+            capture_output=True,
+        )
+
+
+@main.command()
+@click.argument(
+    "output_dir",
+    default=Path.cwd,
+    type=click.Path(file_okay=False, path_type=Path),
+)
+@click.pass_context
+def pdf(context: click.Context, output_dir: Path):
+    if not shutil.which("pandoc"):
+        click.echo("Not installed: pandoc", err=True)
+        raise click.Abort()
+    if not shutil.which("pdflatex"):
+        click.echo("Not installed: pdflatex", err=True)
+        raise click.Abort()
+
+    vault = context.obj["vault"]
+    item = prompt_item(vault)
+    note = get_note(vault, item)
+    output_path = output_dir / f"{note.title}.pdf"
+
+    subprocess.run(
+        ["pandoc", "-f", "markdown", "-t", "pdf", "-o", str(output_path)],
+        input=note.document,
+        text=True,
+        check=True,
+    )
+    click.launch(str(output_path))
+
+
+def prompt_item(vault: str) -> dict:
+    result = subprocess.run(
+        [
+            "op",
+            "item",
+            "list",
+            "--format",
+            "json",
+            "--categories",
+            "Secure Note",
+            "--vault",
+            vault,
+        ],
+        check=True,
+        text=True,
+        capture_output=True,
+    )
+    items = sorted(json.loads(result.stdout), key=itemgetter("title"))
+
+    choices = {str(i + 1): item for i, item in enumerate(items)}
+    for n, item in choices.items():
+        updated_on = datetime.fromisoformat(item["updated_at"]).strftime("%Y-%m-%d")
+        click.echo(f"{n:>2}. {item['title']} ({updated_on})")
+    chosen_n = click.prompt(
+        "Enter number of the one you want to edit",
+        type=click.Choice(list(choices.keys())),
+        show_choices=False,
+    )
+    return choices[chosen_n]
+
+
+def get_note(vault: str, item: dict) -> Note:
+    result = subprocess.run(
+        [
+            "op",
+            "item",
+            "get",
+            item["id"],
+            "--format",
+            "json",
+            "--fields",
+            "notesPlain",
+            "--vault",
+            vault,
+        ],
+        check=True,
+        text=True,
+        capture_output=True,
+    )
+    field = json.loads(result.stdout)
+    value = field["value"].strip()
+    title = item["title"]
+    return Note(value=value, title=title, document=f"# {title}\n\n{value}")
